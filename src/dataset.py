30b021fc8f592a27c1aecacca7afa8c91d1d9fdf
lacykaltgr <freundl0509@gmail.com>
2023-07-30 22:08:26 +0200

    model

--- src/dataset.py
+++ src/dataset.py
@@ -1,40 +1,51 @@
-
-from src.utils import *
-from src.utils import numericalSort
+import numpy as np
 import imageio
+from torch.utils.data import Dataset
+import torch
+from utils import numericalSort
 
 
-def load_images(fnames, crop_size=128, stride=32):
+class CustomDataset(Dataset):
+    def __init__(self, x_data, y_data):
+        self.x_data = x_data
+        self.y_data = y_data
+
+    def __len__(self):
+        return len(self.x_data)
+
+    def __getitem__(self, idx):
+        x = torch.transpose(torch.tensor(self.x_data[idx].astype('uint8'), dtype=torch.float), 0, 2)
+        y = torch.transpose(torch.tensor(self.y_data[idx].astype('uint8'), dtype=torch.float), 0, 2)
+        return x, y
+
+
+def load_images(fnames):
     d_list = []
-    for fname in fnames[:13]:
+    for fname in fnames:
         image_raw = imageio.read(fname)
         image = np.array(image_raw.get_data(0))
-        h, w, c = image.shape
-        n_h = int(int(h/stride))
-        n_w = int(int(w/stride))
-        image = padding(image, w, h, c, crop_size, stride, n_h, n_w)
         d_list.append(image)
-    return np.array(d_list, dtype=np.float32)
+    return d_list
 
 
 def load_dataset():
     import glob
 
     # List of file names of actual Satellite images for traininig
-    filelist_trainx = sorted(glob.glob('/Users/laszlofreund/code/ai/satellite-image-segmentation/data/The-Eye-in-the-Sky-dataset/sat/*.tif'), key=numericalSort)
+    filelist_trainx = sorted(glob.glob('/content/drive/MyDrive/colab/sat-images/The-Eye-in-the-Sky-dataset/sat/*.tif'), key=numericalSort)
     # List of file names of classified images for traininig
-    filelist_trainy = sorted(glob.glob('/Users/laszlofreund/code/ai/satellite-image-segmentation/data/The-Eye-in-the-Sky-dataset/gt/*.tif'), key=numericalSort)
+    filelist_trainy = sorted(glob.glob('/content/drive/MyDrive/colab/sat-images/The-Eye-in-the-Sky-dataset/gt/*.tif'), key=numericalSort)
     # List of file names of actual Satellite images for testing
-    filelist_testx = sorted(glob.glob('/Users/laszlofreund/code/ai/satellite-image-segmentation/data/The-Eye-in-the-Sky-test-data/sat_test/*.tif'), key=numericalSort)
+    filelist_testx = sorted(glob.glob('/content/drive/MyDrive/colab/sat-images/The-Eye-in-the-Sky-test-data/sat_test/*.tif'), key=numericalSort)
 
     # Making array of all the training sat images as it is without any cropping
-    x_train = load_images(filelist_trainx[:13])
-    ytrain = load_images(filelist_trainy[:13])
+    x = load_images(filelist_trainx)
+    y = load_images(filelist_trainy)
     x_test = load_images(filelist_testx)
 
-    x_val = x_train[:-1]
-    y_val = ytrain[-1]
+    x_train = x[:-1]
+    y_train = y[:-1]
+    x_val = [x[-1]]
+    y_val = [y[-1]]
 
-    return x_train, ytrain, x_val, y_val, x_test
-
-
+    return x_train, y_train, x_val, y_val, x_test
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,4 @@
 import numpy as np
-from skimage.transform import resize
-
-def resize(img, stride, n_h, n_w):
-    #h,l,_ = img.shape
-    ne_h = (n_h*stride) + stride
-    ne_w = (n_w*stride) + stride
-
-    img_resized = resize(img, (ne_h ,ne_w))
-    return img_resized
-
-
-    # Padding at the bottem and at the left of images to be able to crop them into 128*128 images for training
 
 def padding(img, w, h, c, crop_size, stride, n_h, n_w):
 
@@ -27,7 +15,7 @@ def padding(img, w, h, c, crop_size, stride, n_h, n_w):
 
     return img_pad
 
-import numpy as np
+
 
 def pad_to_square(image, fill_color=0):
     """
@@ -59,52 +47,6 @@ def pad_to_square(image, fill_color=0):
 
     return padded_image
 
-def add_pixals(img, h, w, c, n_h, n_w, crop_size, stride):
-
-    w_extra = w - ((n_w-1)*stride)
-    w_toadd = crop_size - w_extra
-
-    h_extra = h - ((n_h-1)*stride)
-    h_toadd = crop_size - h_extra
-
-    img_add = np.zeros(((h+h_toadd), (w+w_toadd), c))
-
-    img_add[:h, :w,:] = img
-    img_add[h:, :w,:] = img[:h_toadd,:, :]
-    img_add[:h,w:,:] = img[:,:w_toadd,:]
-    img_add[h:,w:,:] = img[h-h_toadd:h,w-w_toadd:w,:]
-
-    return img_add
-
-
-    # Slicing the image into crop_size*crop_size crops with a stride of crop_size/2 and makking list out of them
-
-def crops(a, crop_size = 128):
-
-    stride = 32
-
-    croped_images = []
-    h, w, c = a.shape
-
-    n_h = int(int(h/stride))
-    n_w = int(int(w/stride))
-
-    # Padding using the padding function we wrote
-    ##a = padding(a, w, h, c, crop_size, stride, n_h, n_w)
-
-    # Resizing as required
-    ##a = resize(a, stride, n_h, n_w)
-
-    # Adding pixals as required
-    a = add_pixals(a, h, w, c, n_h, n_w, crop_size, stride)
-
-    # Slicing the image into 128*128 crops with a stride of 64
-    for i in range(n_h-1):
-        for j in range(n_w-1):
-            crop_x = a[(i*stride):((i*stride)+crop_size), (j*stride):((j*stride)+crop_size), :]
-            croped_images.append(crop_x)
-    return croped_images
-
 
 def rgb_to_onehot(rgb_arr, color_dict):
     num_classes = len(color_dict)
@@ -115,6 +57,7 @@ def rgb_to_onehot(rgb_arr, color_dict):
         arr[:,:,i] = np.all(rgb_arr.reshape( (-1,3) ) == color_dict[i], axis=1).reshape(shape[:2])
     return arr
 
+
 def onehot_to_rgb(onehot, color_dict):
     single_layer = np.argmax(onehot, axis=-1)
     output = np.zeros( onehot.shape[:2]+(3,) )
@@ -122,17 +65,13 @@ def onehot_to_rgb(onehot, color_dict):
         output[single_layer==k] = color_dict[k]
     return np.uint8(output)
 
+
 def to_class_no(y_hot_list):
     y_class_list = []
-
     n = len(y_hot_list)
-
     for i in range(n):
-
         out = np.argmax(y_hot_list[i])
-
         y_class_list.append(out)
-
     return y_class_list
 
 
