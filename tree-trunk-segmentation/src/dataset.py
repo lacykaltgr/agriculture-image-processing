ddcf46626d784c3d61f698e366c991e7024e0f3b
lacykaltgr <freundl0509@gmail.com>
2023-09-16 18:36:22 +0200

    3/2 images

--- tree-trunk-segmentation/src/dataset.py
+++ tree-trunk-segmentation/src/dataset.py
@@ -22,23 +22,57 @@ class XYDataset(Dataset):
         return x, y
 
 
-def load_dataset(root):
-    filelist_trainx = sorted(glob.glob(os.path.join(root, '*.JPG')), key=numericalSort)
+def load_dataset(root, target_size_x=None, target_size_y=None):
+    x_filelist_3_2 = sorted(glob.glob(os.path.join(root, '3_2_images', 'train',  '*.JPG')), key=numericalSort)
+    x_filelist_4_3 = sorted(glob.glob(os.path.join(root, '4_3_images', 'train',  '*.jpg')), key=numericalSort)
 
     x_data, y_data = [], []
 
-    for x_file in filelist_trainx:
+    for x_file in x_filelist_3_2:
         x_filename = os.path.basename(x_file)
         x_serial_number = re.search(r'\d+', x_filename).group()
         y_file = os.path.join(root, f'DSC{x_serial_number}_train.png')
-        #[1568:2080,:,:3]
 
-
-        if os.path.exists(y_file):
-            x_data.append(cv2.imread(x_file))
-            y_data.append(cv2.imread(y_file))
-        else:
+        if not os.path.exists(y_file):
             raise ValueError("Target file does not exist")
-        #resized_image = cv2.resize(self.x_data[idx], self.target_size)
+
+        x = cv2.imread(x_file)
+        y = cv2.imread(y_file)
+
+        w_x, h_x, c_x = x.shape
+        new_w_x = h_x / 3 * 4
+        x1 = x[:, :new_w_x]
+        x2 = x[:, w_x-new_w_x:]
+        y = y[1574:2074, :, :3] # igy pont 500, alapból 512
+        y1 = y[:, :new_w_x]
+        y2 = y[:, w_x-new_w_x]
+
+        x1_resized = cv2.resize(x1, target_size_x)
+        x2_resized = cv2.resize(x2, target_size_x)
+        y1_resized = cv2.resize(y1, target_size_y)
+        y2_resized = cv2.resize(y2, target_size_y)
+
+        x_data.append(x1_resized)
+        y_data.append(y1_resized)
+        x_data.append(x2_resized)
+        y_data.append(y2_resized)
+
+    for x_file in x_filelist_4_3:
+        x_filename = os.path.basename(x_file)
+        x_serial_number = re.search(r'\d+', x_filename).group()
+        y_file = os.path.join(root, f'DSC{x_serial_number}_train.png')
+        if not os.path.exists(y_file):
+            raise ValueError("Target file does not exist")
+
+        x = cv2.imread(x_file)
+        y = cv2.imread(y_file)
+
+        if x.shape != target_size_x:
+            x = cv2.resize(x, target_size_x)
+        if y.shape != target_size_y:
+            y = cv2.resize(y, target_size_y)
+
+        x_data.append(x)
+        y_data.append(y)
 
     return x_data, y_data
